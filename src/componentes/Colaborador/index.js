87b337a064530d4c751497b172126e541f589863
HILTONESJR <bmxsts@gmail.com>
2024-06-17 16:34:33 -0300

    refatorando componente e adicionando botao favoritos

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,7 +1,19 @@
 import './colaborador.css'
-import { AiFillCloseCircle } from 'react-icons/ai';
+import { AiFillCloseCircle, AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
 
-const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+
+
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar }) => {
+
+    function favotirar() {
+        aoFavoritar(colaborador.id)
+    }
+
+    const propsFavorito = {
+        size: 25,
+        onClick: favotirar
+    }
+
     return (<div className="colaborador">
         <AiFillCloseCircle
         size={25} 
@@ -13,6 +25,10 @@ const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
         <div className="rodape">
             <h4>{colaborador.nome}</h4>
             <h5>{colaborador.cargo}</h5>
+            <div className='favoritar'>
+                {colaborador.favorito
+                     ? <AiFillHeart {...propsFavorito} color='#FF0000' /> : <AiOutlineHeart {...propsFavorito}/>}
+            </div>
         </div>
     </div>)
 }
